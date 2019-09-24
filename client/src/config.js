47a88efada62172f90428efcfa99a7d154369398
Paweł Żurawka <pzurawka@gmail.com>
2019-09-24 14:14:36 +0200

    Change config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = '/api';
+export const API_URL = 'http://https://blog-react-app-pawel.herokuapp.com/api';
 export const BASE_URL = 'http://https://blog-react-app-pawel.herokuapp.com';
