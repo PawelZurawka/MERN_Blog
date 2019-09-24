ef2b314e270e85d763756e89fdab53a29804d1df
Paweł Żurawka <pzurawka@gmail.com>
2019-09-24 17:18:00 +0200

    Try to fix heroku

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT,
-  DB: process.env.DB
+  PORT: 8000,
+  DB:
+    'mongodb+srv://pzurawka:bardzotrudnehaslo@cluster0-1yspr.mongodb.net/test?retryWrites=true&w=majority'
 };
