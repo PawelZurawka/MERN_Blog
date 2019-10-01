58713f1b4456ec0d258ab6dadd1967b843385b0c
Paweł Żurawka <pzurawka@gmail.com>
2019-10-01 20:18:25 +0200

    Edit config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,6 @@
-export const API_URL = '/api/';
-export const BASE_URL = 'http://localhost:3000/';
+export const API_URL = process.env.BASE_URL
+  ? process.env.BASE_URL
+  : 'http://localhost:8000/api';
+export const BASE_URL = process.env.BASE_URL
+  ? process.env.BASE_URL
+  : 'http://localhost:3000/';
--- server/config.js
+++ server/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT ? process.env.PORT : 8000,
   DB: process.env.DB
+    ? process.env.DB
+    : 'mongodb+srv://pzurawka:bardzotrudnehaslo@cluster0-1yspr.mongodb.net/test?retryWrites=true&w=majority'
 };
