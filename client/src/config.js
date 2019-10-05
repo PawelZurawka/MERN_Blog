864169a38bda9d3973cc9dd800c3bd25ac1187e0
Paweł Żurawka <pzurawka@gmail.com>
2019-10-05 13:28:14 +0200

    Change config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-export const API_URL = process.env.BASE_URL
-  ? process.env.BASE_URL
+export const API_URL = process.env.API_URL
+  ? process.env.API_URL
   : 'http://localhost:8000/api';
 export const BASE_URL = process.env.BASE_URL
   ? process.env.BASE_URL
