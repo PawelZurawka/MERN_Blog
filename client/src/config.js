596e76dbc4253bc8fcc6ef7b208067526b100d3d
Paweł Żurawka <pzurawka@gmail.com>
2019-10-06 22:12:51 +0200

    Change config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,4 @@
 export const API_URL = process.env.API_URL
   ? process.env.API_URL
   : 'http://localhost:8000/api';
-export const BASE_URL = process.env.BASE_URL
-  ? process.env.BASE_URL
-  : 'http://localhost:3000/';
+export const BASE_URL = 'https://blog-react-app-pawel.herokuapp.com/';
--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT ? process.env.PORT : 8000,
-  DB: process.env.DB ? process.env.DB : 'mongodb://localhost:27017/mernapp'
+  PORT: 8000,
+  DB:
+    'mongodb+srv://pzurawka:bardzotrudnehaslo@cluster0-1yspr.mongodb.net/test?retryWrites=true&w=majority'
 };
