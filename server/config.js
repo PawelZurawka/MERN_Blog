9f8222c0c0c54f5c237ec5f789f4ec624ea2b1f9
Paweł Żurawka <pzurawka@gmail.com>
2019-10-24 12:32:44 +0200

    Change config files

--- server/config.js
+++ server/config.js
@@ -1,13 +1,5 @@
-// module.exports = {
-//   PORT: 8000,
-//   DB:
-//     'mongodb+srv://pzurawka:bardzotrudnehaslo@cluster0-1yspr.mongodb.net/test?retryWrites=true&w=majority'
-// };
-
-const dotenv = require('dotenv');
-dotenv.config();
-
 module.exports = {
-  PORT: process.env.PORT,
-  DB: process.env.DB
+  PORT: 8000,
+  DB:
+    'mongodb+srv://pzurawka:bardzotrudnehaslo@cluster0-1yspr.mongodb.net/test?retryWrites=true&w=majority'
 };
