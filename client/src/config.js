e658680fc0ea70409762751ead78aa643878e877
Paweł Żurawka <pzurawka@gmail.com>
2019-10-24 12:15:25 +0200

    Change config files

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,7 @@
-export const API_URL = process.env.API_URL
-  ? process.env.API_URL
-  : 'http://localhost:8000/api';
-export const BASE_URL = 'https://blog-react-app-pawel.herokuapp.com/';
+// export const API_URL = process.env.API_URL
+//   ? process.env.API_URL
+//   : 'http://localhost:8000/api';
+// export const BASE_URL = 'https://blog-react-app-pawel.herokuapp.com/';
+
+export const API_URL = '/api';
+export const BASE_URL = 'https://blog-react-app-pawel.herokuapp.com';
