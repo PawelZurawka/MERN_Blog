02b8746f6e62804812b67127f6d84ccba20a8395
Paweł Żurawka <pzurawka@gmail.com>
2019-11-09 17:57:25 +0100

    Change gitignore

---
+++ client/build/precache-manifest.26f47e5d5abfc8e2d796e90242967e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a1b4d55e32f68faac6180c28485f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd7770eebb99a3aec6ab",
+    "url": "/static/css/2.ee498ddd.chunk.css"
+  },
+  {
+    "revision": "473171a2fcffc579960b",
+    "url": "/static/css/main.d5c6ed4f.chunk.css"
+  },
+  {
+    "revision": "cd7770eebb99a3aec6ab",
+    "url": "/static/js/2.b6752033.chunk.js"
+  },
+  {
+    "revision": "473171a2fcffc579960b",
+    "url": "/static/js/main.d18c4888.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  }
+]);
