e7f475352642353e72ebe727b5f08ae1027bf30b
Paweł Żurawka <pzurawka@gmail.com>
2019-11-09 19:52:13 +0100

    edit config

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
-//export const API_URL = '/api';
-
-export const API_URL = process.env.API_URL
-  ? process.env.API_URL
-  : 'http://localhost:8000/api';
+export const API_URL = '/api';
 export const BASE_URL = 'https://blog-mernapp.herokuapp.com/';
+
+//local server
+//export const API_URL = 'http://localhost:8000/api';
+//export const BASE_URL = 'http://localhost:3000';
