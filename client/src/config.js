2f103b1e34e8110c3886dd948dba30d4b007977b
Paweł Żurawka <pzurawka@gmail.com>
2019-11-09 16:37:07 +0100

    Try to fix heroku

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,4 @@
-export const API_URL = '/api';
+export const API_URL = process.env.API_URL
+  ? process.env.API_URL
+  : 'http://localhost:8000/api';
 export const BASE_URL = 'https://blog-react-app-pawel.herokuapp.com/';
